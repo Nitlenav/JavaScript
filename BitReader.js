c8d9325b321f967244a2d30c198849fe4cc46a40
Aleksandr <Nitlenav@gmail.com>
2017-03-08 17:52:07 +0300

    Выполнил задание согласно условию.

--- BitReader.js
+++ BitReader.js
@@ -1,23 +1,20 @@
 
+"use strict";
 
-var bit = '01010101010'; // Сюда вводим число
-var arayBit = bit.split('');
-arayBit.reverse();
-var numb = 0;
-var a = arayBit.indexOf('2');
-var b = arayBit.indexOf('3');
-var c = arayBit.indexOf('4');
-var d = arayBit.indexOf('5');
-var f = arayBit.indexOf('6');
-var g = arayBit.indexOf('7');
-var h = arayBit.indexOf('8');
-var i = arayBit.indexOf('9');
-if (a <= -1 && b <= -1 && c <= -1 && d <= -1 && f <= -1 && g <= -1 && h <= -1 && i <= -1)
+function convert (source)               //обьявляем функцию с принимаемым параметром
 {
-console.log(parseInt(bit, 2).toString()); //Самый простой способ
+let arayBit = source.split('');         //параметр переводим в массив символов
+arayBit.reverse();            //инвертируем массив символов
+
+for(let i = 0; i < arayBit.length; i++){ //в цикле перебираетм масив символов
+
+if (arayBit[i] == 1 || arayBit[i] == 0){} //проверяем на наличие символов 1 или 0 и ничего не делаем
+else{return undefined;}                   //выбрасывае  ошибку undefined 
 }
-else 
-{ 
-var str = parseInt(bit).toString(2);
-console.log(str);
+
+return parseInt(source, 2).toString(); //Самый простой способ
+//return console.log(parseInt(source).toString(2));
 }
+
+let num =  convert('01010101010');
+console.log(num);
