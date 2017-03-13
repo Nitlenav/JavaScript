040c4199f377405bce997d5b0e84a900e2c2bcd0
Aleksandr <Nitlenav@gmail.com>
2017-03-14 00:18:28 +0300

    Решение первой задачи

---
+++ 11.03.16/task_1.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function readString(value){
+
+   let message = {failure: 'Сторка пуста;('};
+    let a = value.split('');
+    let count = 0;
+    let strin = "";
+  if (a.length === 0){ throw new Error(message.failure); }
+    else { let counrStr ='';
+        for(let i = 0; i < a.length;i++ ){
+        counrStr += a[i];
+        let str = new RegExp(counrStr, "g");
+        let num = value.match(str).length
+        if ( num >= count && counrStr.length > strin.length){
+            count = num;
+            strin = counrStr;
+        }
+        }
+    }
+console.log('Строка ' + strin + ' встречается '+ count + ' раза.');
+}
+
+ readString('abcabcabcabc');
