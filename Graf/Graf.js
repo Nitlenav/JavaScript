f1b01936ca65887e8d8df5c82803c940fdadd034
Aleksandr <Nitlenav@gmail.com>
2017-04-15 20:10:39 +0300

    Jбновление проекта

--- Graf/Graf.js
+++ Graf/Graf.js
@@ -1,51 +1,122 @@
 'use strict'
 
+function Graf(arr){
+    let x, y;
+    let obj; 
 
-
-class Graf {
-    
-constructor (tops){
- console.log(tops[0]);
- console.log(tops[1]);   
- let message = {failure: 'Данное ребро присутствует'};
-//this.node [] = tops[0], tops[1];
-let masGraf = new Map ();
-
-for(let countMas of tops){
-let top = countMas[0];
-let rev = countMas[1];
-console.log(top);
-console.log(rev);
-
-if(!masGraf.has(top))
-{
-let masVal = [];
-masGraf.set(top, masVal.push[rev])
-console.log(masGraf)
-}
-else{
-/*let masValueMap = masGraf.get(top)
-for(let val of masValueMap){
-    if (val === rev){ 
-throw new Error(message.failure);
+    for(let i = 0; i<arr.length; i++){
+        x = arr[i][0];
+        y = arr[i][1]; 
+        
+        obj = couple(x, y); 
     }
-    else{
-        masGraf.set(top, masValueMap.push[rev])
-        console.log(masGraf)
+    return obj; 
+}
+
+let calculated = {}; 
+
+function couple(x, y){
+
+    let couple1, couple2; 
+   
+    if(calculated[x] && calculated[y]){
+        couple1 = calculated[x];
+        couple2 = calculated[y];
+    }else if(calculated[x]){
+        couple2 = new Node(y); 
+        couple1 = calculated[x]; 
+    }
+    else if(calculated[y]){
+        couple1 = new Node(x); 
+        couple2 = calculated[y];
+    }else{
+        couple1 = new Node(x); 
+        couple2 = new Node(y); 
     }
     
-}*/
- masGraf.set(top, masVal.push[rev])
+    couple1.partners.push(couple2); 
+    couple2.partners.push(couple1); 
+    
+    calculated[couple1.value] = couple1;
+    calculated[couple2.value] = couple2;
+
+    return calculated; 
+
 }
+
+function Node(value){
+    this.value = value; 
+    this.partners = []; 
 }
-}    
+
+
+function searchDeep(obj){
+    let roll;
+    let searched = [];
+    let searchedChild;  
+
+    for(let key in obj){
+        roll = obj[key]; 
+        searched = smartMix(searched, [].push(roll.value)); 
+        searchedChild = deepDeep(roll.partners); 
+
+        searched = smartMix(searched, searchedChild)
+    }
+        
+    return searched; 
+}
+
+let proxyCalculated = [];
+
+function deepDeep(arr){    
+    let roll;  
+
+    for(let j=0; j<arr.length; j++){
+        roll = arr[j];
+        if(proxyCalculated.indexOf(roll.value) == -1){
+            proxyCalculated.push(roll.value); 
+            deepDeep(roll.partners)
+        }
+    }
+
+    return proxyCalculated; 
+
 }
 
 
-let graf = new Graf ([[0,2], [2,6], [0,3] ]);
+function smartMix(where, what){
+   
+    for(let i=0; i<what.length; i++){
+        if(where.indexOf(what[i]) == -1){
+            where.push(what[i]); 
+        }
+    }
 
-//graf.dfs(graf.nodes[0]);
+    return where; 
+}
 
-// другой вариант графа
-//let graf = new Graf ([[0,3],[1,3],[2,3],[4,3],[5,4]]);
-// graf.dfs(graf.nodes[4]);
+    /*
+        поиск в ширину
+    */
+function searchWidth(obj){
+    let listOfRoute = []; 
+
+    for(key in obj){
+        let roll = obj[key]; 
+
+        if(roll.partners.length==1){
+            listOfRoute.push("Из вершины " + roll.value + " можно прийти в вершину " + roll.partners[0].value); 
+        }else if(roll.partners.length>1){
+            let str = "Из вершины " + roll.value + " можно прийти в вершины ";
+            let map = "";
+            for(let i=0; i<roll.partners.length; i++){
+                map += roll.partners[i].value + ", "
+            }
+             map = map.substring(0, map.length-2); 
+             str = str + map + "."
+             listOfRoute.push(str); 
+        }
+    }
+
+    return listOfRoute; 
+} 
